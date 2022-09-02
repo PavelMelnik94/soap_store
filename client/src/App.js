96090327cd40e6164be337dc9fe41f2e13f4e421
PavelMelnik94 <p.melnik@intelico.su>
2022-09-02 15:33:22 +0400

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
-import './App.scss';
 import { Helmet } from "react-helmet";
-
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./components/AppRouter";
 function App() {
   return (
-    <>
+    <BrowserRouter >
       <Helmet>
         <meta charSet="utf-8" />
         <title>My Title</title>
         <link rel="canonical" href="http://mysite.com/example" />
       </Helmet>
-      <div className="App">
-
-      </div>
-    </>
+      <AppRouter />
+    </BrowserRouter>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import '.styles/index.scss';
+import './styles/index.scss';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  ReactDOM.hydrate(<App />, rootElement);
-} else {
-  ReactDOM.render(<App />, rootElement);
-}
+// const rootElement = document.getElementById("root");
+// if (root.hasChildNodes()) {
+//   ReactDOM.hydrate(<App />, root);
+// } else {
+//   ReactDOM.render(<App />, root);
+// }
 
