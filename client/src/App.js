65150e232a4394aa289913d7c27d3586128b0c4a
Paveldev <recyclesordie94@gmail.com>
2022-09-04 16:15:43 +0400

    registration\login fns

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,47 @@ import { Helmet } from "react-helmet";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import NavBar from "./components/temp/Navbar";
+import {observer} from "mobx-react-lite";
+import {useContext, useEffect, useState} from "react";
+import {Context} from "./index";
+import {check} from "./http/userApi";
+import {Spinner} from "react-bootstrap";
 
 function App() {
+
+    const {user} = useContext(Context);
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+       setTimeout(() => {
+           check().then(data => {
+               user.setUser(true)
+               user.setIsAuth(true)
+           } ).finally(() => setLoading(false))
+       }, 2000)
+    }, [])
+
+    if (loading) {
+        console.log('loading')
+        return (
+            <div style={{width: '100%', height: '100vh'}} className="d-flex justify-content-center align-items-center">
+                <Spinner animation="grow" />
+            </div>
+        )
+    }
   return (
     <BrowserRouter >
+
       <Helmet>
         <meta charSet="utf-8" />
         <title>My Title</title>
         <link rel="canonical" href="http://mysite.com/example" />
       </Helmet>
-
         <NavBar />
-
       <AppRouter />
 
     </BrowserRouter>
   );
 }
 
-export default App;
+export default observer(App);
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, {useContext, useState} from 'react'
 import {Button, Card, Container, Form, Row} from "react-bootstrap";
-import {NavLink, useLocation} from "react-router-dom";
-import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils";
+import {NavLink, useHistory, useLocation} from "react-router-dom";
+import {LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE} from "../utils";
 import {observer} from "mobx-react-lite";
+import {login, registration} from "../http/userApi";
+import {Context} from "../index";
 
 const Auth = () => {
+    const {user} = useContext(Context);
 
     const location = useLocation();
+    const history = useHistory();
     const isLogin = location.pathname === LOGIN_ROUTE;
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const click = async () => {
+        try {
+            let data;
+            if (isLogin) {
+                data = await login(email, password)
+            } else {
+                data = await registration(email, password)
+            }
+
+            user.setUser(data)
+            user.setIsAuth(true)
+            history.push(SHOP_ROUTE)
+        } catch (e) {
+            alert(e.response.data.message)
+        }
+    }
 
     const renderRegistrationText = () => {
         return (
@@ -38,15 +61,21 @@ const Auth = () => {
                     <Form.Control
                         className={"mt-3"}
                         placeholder="Введите ваш email..."
+                        value={email}
+                        type="email"
+                        onChange={(e) => setEmail(e.target.value)}
                         />
                     <Form.Control
                         className={"mt-3"}
                         placeholder="Введите ваш password..."
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
 
                     <Row className="d-flex justify-content-between align-items-center  mt-3">
                         {isLogin ? renderRegistrationText() : renderExistAccountText()}
-                        <Button className="col-4" style={{width: "fit-content", height: "fit-content"}} variant={"outline-success"}>
+                        <Button onClick={() => click()} className="col-4" style={{width: "fit-content", height: "fit-content"}} variant={"outline-success"}>
                             {isLogin ? 'Войти' : "Регистрация"}
                         </Button>
                     </Row>
